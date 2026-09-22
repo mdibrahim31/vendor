e58c1254500386f15d8728b274e4949da471e7e0
mdibrahim31 <mdibrahim.n.2018@gmail.com>
2026-09-22 07:08:09 +0600

    Update home.js

--- home.js
+++ home.js
@@ -8,7 +8,7 @@ const vendorId = localStorage.getItem('vendor_id');
 
 // Jodi vendor login na kore direct home.html-e ashe, tahole login page-e pathiye dibe
 if (!vendorId) {
-    window.location.href = 'index.html;
+    window.location.href = 'index.html';
 }
 
 // Page load hole vendor er data fetch kora
